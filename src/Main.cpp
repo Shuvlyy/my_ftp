3043f14a221b65427eacdba9e6b2c28bb4a24b12
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-02-23 19:18:46 +0100

    feat: Drafted main.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,12 @@
+#include "Server/Server.hpp"
+
+#include "Exception/AException.hpp"
+
+#include "Parser/Parser.hpp"
+
+#define SH_EXIT_SUCCESS 0
+#define SH_EXIT_FAILURE 84
+
 int
 main
 (
@@ -5,5 +14,21 @@ main
     char *argv[]
 )
 {
-    return 0;
+    try {
+        ftp::Parser parser(argc, argv);
+
+        if (parser.processFlags()) {
+            return SH_EXIT_SUCCESS;
+        }
+
+        ftp::Server server(parser.getPort(), parser.getPath());
+
+        server.start();
+    }
+    catch (ftp::exception::IException &exception) {
+        std::cerr << exception;
+        return SH_EXIT_FAILURE;
+    }
+
+    return SH_EXIT_SUCCESS;
 }
