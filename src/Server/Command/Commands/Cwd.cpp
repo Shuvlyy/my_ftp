2f7f6a070262ca6d89c6f45cca1f91212e454b87
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-02-27 18:27:37 +0100

    fix(command>cwd): Now properly handles exceptions.

--- src/Server/Command/Commands/Cwd.cpp
+++ src/Server/Command/Commands/Cwd.cpp
@@ -1,4 +1,8 @@
 #include "Server/Command/Commands/Cwd.hpp"
+
+#include "Exception/Exceptions/PathIsNotDir.hpp"
+#include "Exception/Exceptions/WdOutOfScope.hpp"
+
 #include "Server/Server.hpp"
 
 #include "Common/Responses.hpp"
@@ -23,6 +27,14 @@ ftp::server::commands::Cwd::execute
 )
     const
 {
-    session.cwd(commandArguments.at(0));
-    clientSocket.send(RES_FILE_ACTION_REQ);
+    try {
+        session.cwd(commandArguments.at(0));
+        clientSocket.send(RES_FILE_ACTION_REQ);
+    }
+    catch (exception::PathIsNotDir &exception) {
+        clientSocket.send("dis not a dir mate");
+    }
+    catch (exception::WdOutOfScope &exception) {
+        clientSocket.send("out of scope (no permission)!!");
+    }
 }
