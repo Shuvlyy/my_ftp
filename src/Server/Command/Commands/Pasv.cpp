ef7dc1863ab44d03fa8dd6de946efe0cc6e403de
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-03-02 18:25:22 +0100

    feat(command>pasv): Drafted the `PASV` command. Still under work i guess.

--- src/Server/Command/Commands/Pasv.cpp
+++ src/Server/Command/Commands/Pasv.cpp
@@ -1,6 +1,12 @@
 #include "Server/Command/Commands/Pasv.hpp"
 
+#include "Common/Responses.hpp"
+
 #include "Server/Server.hpp"
+
+#include <format>
+#include <sstream>
+#include <arpa/inet.h>
 
 bool
 ftp::server::commands::Pasv::isUsageValid
@@ -10,6 +16,22 @@ ftp::server::commands::Pasv::isUsageValid
     const
 {
     return commandArguments.empty();
+}
+
+static std::vector<int>
+parseIp
+(
+    const std::string &ip
+)
+{
+    std::vector<int> tokens;
+    std::istringstream stream(ip);
+    std::string currentToken;
+
+    while (std::getline(stream, currentToken, '.')) {
+        tokens.push_back(std::stoi(currentToken)); // FIXME: A bit ugly.
+    }
+    return tokens;
 }
 
 void
@@ -22,6 +44,19 @@ ftp::server::commands::Pasv::execute
 )
     const
 {
-    // ...
-    clientSocket.send("Executed");
+    DataSocket &dataSocket = session.getDataSocket();
+
+    dataSocket.closeSocket();
+    dataSocket = DataSocket();
+
+    const char *serverIp = inet_ntoa(dataSocket.getAddress().sin_addr);
+    const uint16_t port = ntohs(dataSocket.getAddress().sin_port);
+    std::vector<int> ipTokens = parseIp(serverIp);
+
+    // "227 Entering Passive Mode ({},{},{},{},{},{})."
+    clientSocket.send(std::format(
+        RES_ENTERING_PASSIVE_MODE,
+        ipTokens.at(0), ipTokens.at(1), ipTokens.at(2), ipTokens.at(3),
+        port / 256, port % 256
+    ));
 }
