a5ac5d73301be8e8b5af3fdfda15dfda04d9427e
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-03-10 15:53:44 +0100

    feat(commands): Drafted `STOR` command.

---
+++ include/Server/Command/Commands/Stor.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Server/Command/ACommand.hpp"
+
+namespace ftp::server::commands
+{
+
+    class Stor final
+        : public ACommand
+    {
+    public:
+        [[nodiscard]] bool isUsageValid(
+            const std::vector<std::string> &commandArguments
+        ) const override;
+
+        void execute(
+            Server *server,
+            const std::vector<std::string> &commandArguments,
+            Socket &clientSocket,
+            Session &session
+        ) const override;
+
+        [[nodiscard]] std::string getCommandName()
+            const override
+        {
+            return "STOR";
+        }
+
+        [[nodiscard]] std::string getCommandDescription()
+            const override
+        {
+            return "";
+        }
+
+        [[nodiscard]] std::string getCommandSyntax()
+            const override
+        {
+            return "STOR [<pathname>]";
+        }
+    };
+
+}
