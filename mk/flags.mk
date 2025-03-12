ffc7ea4e860cf4ab3487a02e79037cb287ba8259
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-03-12 20:17:54 +0100

    feat(build): Now includes and uses YML parser.

--- mk/flags.mk
+++ mk/flags.mk
@@ -10,3 +10,8 @@ CFLAGS += -Wextra
 #CFLAGS += -Werror
 CFLAGS += -std=c++20
 CFLAGS += -iquote include
+
+CFLAGS += -iquote yml_parser/include
+CFLAGS += -L yml_parser -lyml
+
+#LDFLAGS += -lncurses
